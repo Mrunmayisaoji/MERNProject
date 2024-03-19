3b55d04e389f2a309472bdd04a9ba6b7b4b4cc85
Mrunmayisaoji <mrunmayisaoji@gmail.com>
2024-03-19 15:07:35 -0600

    init

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,33 @@
 require('dotenv').config()
 
 const express = require('express')
+const workoutRoutes = require('./routes/workouts')
+const mongoose = require('mongoose')
 
 //express app
 const app = express()
 
-
-app.get('/', (reg, res)=> {
-    res.json({msg: 'Welcome to the app'})
+//middleware
+app.use(express.json())
+app.use((req, res, next) =>{
+    console.log(req.path, req.method)
+    next()
 })
+
+//routes
+app.use('/api/workouts',workoutRoutes)
+
+
+//connect to DB
+mongoose.connect(process.env.MONG_URI)
+    .then(()=> {
+        app.listen(process.env.PORT, () => {
+            console.log('Connected to DB and Listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
 //listen for request
-app.listen(process.env.PORT, () => {
-    console.log('Listening on port', process.env.PORT)
-})
+
